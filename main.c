cb1a28f04c30559b1179b86a5c3f4c28583f7659
Patrick Conroy <patrick@conroy-family.net>
2015-12-16 14:02:40 -0700

    Test Commit

--- main.c
+++ main.c
@@ -15,6 +15,7 @@
  * 02-Dec-2015  - extensive cleanup - pulling in more common logging, MQTT and INI file libraries
  * 08-Dec-2015  - messing around with Git and AWS Code Commit
  * 14-Dec-2015  - still messing around with Git and AWS Code Commit
+ * 16-Dec-2015  - still messing around with Git and AWS Code Commit
  */
 #define _POSIX_SOURCE
 
