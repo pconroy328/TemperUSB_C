3bcabecbbafb7248e6ff09748d99dbc74761418f
Patrick Conroy <patrick@conroy-family.net>
2022-03-29 09:08:47 -0600

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+###
+sudo cp temperusb.service /etc/systemd/system/.
+sudo chmod 644 /etc/systemd/system/temperusb.service
+sudo systemctl daemon-reload
+sudo systemctl start temperusb
+sudo systemctl status temperusb
+sudo systemctl enable temperusb
+
